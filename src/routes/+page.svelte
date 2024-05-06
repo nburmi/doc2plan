1c53036f67a812c772fb20eb486da43ae8ca8e9f
Nikita <nickburmi@gmail.com>
2024-05-06 21:41:40 +0400

    feat: index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,61 @@
 <script lang="ts">
+    import { base } from '$app/paths';
+    import { goto } from '$app/navigation';
+
 </script>
 
 
-<h1>Page</h1>
+
+<div class="container max-w-5xl mx-auto p-4 rounded-lg mt-5 space-y-4">
+    <h2 class="h2">
+        Welcome to doc2plan, where you transform documents into dynamic learning experiences. 
+    </h2>
+    <h3 class="h3">
+        Create, view, and manage your learning plans seamlessly in your browser.
+    </h3>
+
+    <div class="card bg-gradient-to-br variant-gradient-secondary-tertiary">
+        <header class="card-header">
+            <h3 class="h3">Creator</h3>
+        </header>
+        <section class="p-4">
+            <p>Start by creating your personalized learning plan. 
+                Use AI-powered tools to extract key content from documents or manually set up your topics and quizzes.
+            </p>
+        </section>
+        <footer class="card-footer">
+            <!-- redirect to creator -->
+            <button class="btn variant-filled" on:click={() => goto(`${base}/creator`)}>Explore Creator</button>
+        </footer>
+    </div>
+
+
+    <!-- Viewer -->
+    <div class="card bg-gradient-to-br variant-gradient-success-warning text-white">
+        <header class="card-header">
+            <h3 class="h3">Viewer</h3>
+        </header>
+        <section class="p-4">
+            <p>View your learning plan and track your progress.</p>
+        </section>
+        <footer class="card-footer">
+            <!-- redirect to viewer -->
+            <button class="btn variant-filled" on:click={() => goto(`${base}/viewer`)}>Explore Viewer</button>
+        </footer>
+    </div>
+
+
+    <!-- Settings -->
+    <div class="card bg-gradient-to-br variant-gradient-error-warning">
+        <header class="card-header">
+            <h3 class="h3">Settings</h3>
+        </header>
+        <section class="p-4">
+            <p>Customize your learning experience by adjusting settings.</p>
+        </section>
+        <footer class="card-footer">
+            <!-- redirect to settings -->
+            <button class="btn variant-filled" on:click={() => goto(`${base}/settings`)}>Explore Settings</button>
+        </footer>
+    </div>
+</div>
