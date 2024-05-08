ecd2807c4ae3a9122eccd697f63a0ac9a33d6c43
Nikita <nickburmi@gmail.com>
2024-05-08 21:45:42 +0400

    refactor: Update eslint configuration and add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,53 @@
-// @ts-check
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import js from '@eslint/js';
+import svelteParser from 'svelte-eslint-parser';
+import tsEslint from 'typescript-eslint';
+import tsParser from '@typescript-eslint/parser';
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended);
+export default [
+  js.configs.recommended,
+  ...tsEslint.configs.strict,
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  eslintPluginPrettierRecommended, // must be last to override conflicting rules.
+  {
+    rules: {
+      semi: ['warn', 'always'],
+      quotes: [
+        'warn',
+        'single',
+        { avoidEscape: true, allowTemplateLiterals: true },
+      ],
+      'no-nested-ternary': 'error',
+      'linebreak-style': ['error', 'unix'],
+      'no-cond-assign': ['error', 'always'],
+      'no-console': 'error',
+      '@typescript-eslint/sort-type-constituents': 'error',
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true,
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+    },
+    rules: {
+      'svelte/no-target-blank': 'error',
+      'svelte/no-at-debug-tags': 'error',
+      'svelte/no-reactive-functions': 'error',
+      'svelte/no-reactive-literals': 'error',
+    },
+  },
+];
