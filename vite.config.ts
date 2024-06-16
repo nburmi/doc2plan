03bb285d6bb6613e3200c2cf44a94cdc76c741a2
Nikita <nickburmi@gmail.com>
2024-06-16 20:50:53 +0400

    build: Disable minification in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [sveltekit(), purgeCss()],
+	build: {
+		minify: false,
+	}
 });
